c1d051c6aa4be9232d09ddadcbbf5cd639a74759
SIPHA PHORN <SIPHA PHORN@SIPHAPHORN>
2019-10-26 10:34:55 +0700

    Message Sipha lol

---
+++ testGitAspNet/Controllers/ProvincesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using testGitAspNet.Models;
+
+namespace testGitAspNet.Controllers
+{
+    public class ProvincesController : Controller
+    {
+        private DB db = new DB();
+
+        // GET: Provinces
+        public ActionResult Index()
+        {
+            var provinces = db.Provinces.Include(p => p.Country);
+            return View(provinces.ToList());
+        }
+
+        // GET: Provinces/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = db.Provinces.Find(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            return View(province);
+        }
+
+        // GET: Provinces/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name");
+            return View();
+        }
+
+        // POST: Provinces/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,CountryId")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Provinces.Add(province);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", province.CountryId);
+            return View(province);
+        }
+
+        // GET: Provinces/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = db.Provinces.Find(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", province.CountryId);
+            return View(province);
+        }
+
+        // POST: Provinces/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,CountryId")] Province province)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(province).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "Id", "Name", province.CountryId);
+            return View(province);
+        }
+
+        // GET: Provinces/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Province province = db.Provinces.Find(id);
+            if (province == null)
+            {
+                return HttpNotFound();
+            }
+            return View(province);
+        }
+
+        // POST: Provinces/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Province province = db.Provinces.Find(id);
+            db.Provinces.Remove(province);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
